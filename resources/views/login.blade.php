71b42d148fd8d99751f8bcf58fbc605076cd2b6a
derek <github@derekgillett.com>
2016-01-20 09:37:50 +1100

    Fix login links

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,12 +7,12 @@
             echo 'Hi '. $user->name.'!&nbsp;';
             echo Html::link('', 'Home').'&nbsp';
             echo Html::link('/admin/invoice', 'Invoicing').'&nbsp';
-            echo Html::link('/auth/logout', 'Logout');
+            echo Html::link('/logout', 'Logout');
         }
         else
         {
-            echo Html::link('/auth/register', 'Register').'&nbsp';
-            echo Html::link('/auth/login', 'Login');
+            echo Html::link('/register', 'Register').'&nbsp';
+            echo Html::link('/login', 'Login');
         }
         ?>
     </p>
