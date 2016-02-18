3e75256db495078d41d96cb9a19b3c26f11a6277
derek <github@derekgillett.com>
2016-02-19 09:41:13 +1100

    create emails table

--- database/migrations/force_drop.sql
+++ database/migrations/force_drop.sql
@@ -1,4 +1,5 @@
 use homestead;
+drop table if exists emails;
 drop table if exists invoice_items;
 drop table if exists invoices;
 drop table if exists invoice_item_categories;
@@ -9,4 +10,6 @@ drop table if exists role_user;
 drop table if exists users;
 drop table if exists roles;
 drop table if exists settings;
+drop table if exists jobs;
+drop table if exists failed_jobs;
 drop table if exists migrations;
